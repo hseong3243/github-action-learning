92485c2a9cebacca4ea2d301531c96f10b21b9d6
hseong3243 <hseong3243@gmail.com>
2023-08-15 23:56:00 +0900

    chore: github actions 테스트 리포트 생성 추가

--- src/test/java/com/example/githubaction/GitHubActionsTest.java
+++ src/test/java/com/example/githubaction/GitHubActionsTest.java
@@ -1,11 +1,26 @@
 package com.example.githubaction;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class GitHubActionsTest {
 
     @Test
-    void generateTestReport() {
+    void generateTestResult() {
         //empty
+    }
+
+    @Test
+    void generateTestReport() {
+        //success
+    }
+
+    @Test
+    void generateTestReport_ButFail() {
+        String fail = "fail";
+
+        assertThat(fail).isNotEqualTo("fail");
     }
 }
